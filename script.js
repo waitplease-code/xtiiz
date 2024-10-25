9f387714770d4d26b4f90bac7376c5641e107323
waitplease-code <herr.nicholas.ott2@gmail.com>
2024-10-24 22:05:46 -0300

    TRANSITION IN!!!! Looks really cool and I like it a lot. Basic Structure set up, needs styling + missing some elements.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const logo = document.getElementById('PICLOGO');
+/**const logo = document.getElementById('PICLOGO');
 
 logo.addEventListener("mouseover", (event) => {
     logo.src="images/logoGIF.gif";
@@ -6,6 +6,18 @@ logo.addEventListener("mouseover", (event) => {
     setTimeout(() => {
         logo.src="images/LOGO.jpg";
     }, 4000)
+});*/
+
+console.log(1);
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        entry.target.classList.toggle('show', entry.isIntersecting);
+    });
 });
 
-
+const dog = document.querySelectorAll('.hidden'); 
+dog.forEach((el) => observer.observe(el));
