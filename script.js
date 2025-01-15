2e90cd60a8fc2d719c88072beb7d2de5481036b6
waitplease-code <herr.nicholas.ott2@gmail.com>
2025-01-15 19:01:51 -0300

    aghhhh

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+            
         }
     });
 });
